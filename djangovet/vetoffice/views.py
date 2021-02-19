75e418108ace56e25614d1bef454db37dd98618e
Jacqueline Wilson <jackie@ewilsons.com>
2021-02-18 21:47:08 -0500

    c4 first template exercise

--- djangovet/vetoffice/views.py
+++ djangovet/vetoffice/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+pets = [
+   { 'petname': 'Fido', 'animal_type': 'dog'},
+   { 'petname': 'Clementine', 'animal_type': 'cat'},
+   { 'petname': 'Cleo', 'animal_type': 'cat'},
+   { 'petname': 'Oreo', 'animal_type': 'dog'},
+]
+
 # Create your views here.
 def home(request):
-   return render(request, 'vetoffice/home.html')
+   context = {"name": "<Put your name here>", "pets": pets}
+   return render(request, 'vetoffice/home.html', context)
